c8d01d82292493cbdd4a69371be8765acb8affa2
Brett Borchardt <bborchardt@gmail.com>
2013-07-04 13:15:06 -0500

    initial project

---
+++ app/web/js/pw.js
@@ -0,0 +1,67 @@
+var pw = (function(document, $) {
+    var showMD5 = function() {
+        var input = getInput();
+        var hash = CryptoJS.MD5(input);
+        var password = hash.toString(CryptoJS.enc.Hex);
+        var truncated = truncate(password);
+        setOutput(truncated);
+    };
+
+    var showPassword = function() {
+        var input = getInput();
+        var hash = CryptoJS.MD5(input);
+        var password = hash.toString(CryptoJS.enc.Base64);
+        password = password.replace(/\+/g, "7");
+        password = password.replace(/\//g, "2");
+        password = password.replace(/=/g, "4");
+        var truncated = truncate(password);
+        var strong = strengthen(truncated);
+        setOutput(strong);
+    };
+
+    var getInput = function() {
+        return $("#passwordin").val();
+    };
+
+    var getLength = function() {
+        return $("#passwordlength").val();
+    };
+
+    var truncate = function(text) {
+        var length = parseInt(getLength());
+        if(length > 0 && length < text.length) {
+            text = text.substring(0, length)
+        }
+        return text;
+    };
+
+    var strengthen = function(text) {
+        if(text.length >= 6) {
+            if(!text.match(/[0-9].*[0-9]/g)) {
+                text = "9" + text.substring(0, text.length - 2) + "3";
+            }
+            if(!text.match(/[A-Z].*[A-Z]/g)) {
+                text = "N" + text.substring(0, text.length - 2) + "W";
+            }
+            if(!text.match(/[a-z].*[a-z]/g)) {
+                text = "q" + text.substring(0, text.length - 2) + "b";
+            }
+        }
+        return text;
+    };
+
+    var setOutput = function(output) {
+        var e = $("#passwordout");
+        e.val(output);
+        e.select();
+    };
+
+    $(document).ready(function() {
+        $("#passwordin").focus();
+    });
+
+    return {
+        showMD5: showMD5,
+        showPassword: showPassword
+    }
+})(document, jQuery);
